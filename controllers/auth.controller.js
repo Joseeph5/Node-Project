0ef44b8872fa525b59c53149d3a575dfafb1f9d1
Youssef Benamor <youssef.beenamor@gmail.com>
2024-06-07 18:53:16 +0100

    add auth.controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,40 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Authentication failed. User not found." });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ message: "Authentication failed. Wrong password." });
+    }
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
